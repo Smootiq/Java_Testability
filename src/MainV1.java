17edef02012233dc2aec4de5b81d41a1d5dd0d8f
Gulyuki Aleksey <gulyuki_a@bk.ru>
2020-04-14 19:42:00 +0300

    Change .java files

--- src/MainV1.java
+++ src/MainV1.java
@@ -2,7 +2,7 @@ public class MainV1 {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
-        int miles = (int) service.calculate(price);
+        int miles = service.calculate(price);
         System.out.println(miles);
     }
 }
